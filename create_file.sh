161f34a208dbdb5175b246b12235812ab53264a0
Dennis Uhlemann <dennis.uhlemann@web.de>
2022-04-29 15:46:41 +0000

    Bash

--- create_file.sh
+++ create_file.sh
@@ -6,3 +6,9 @@ touch "$dir"/"$file"
 echo '#!/bin/bash' > $dir/$file
 echo '#Purpose:' >> $dir/$file
 echo '#Created Date:' `date` >> $dir/$file
+echo '#Author: Dennis Uhlemann' >> $dir/$file
+echo '# START #' >> $dir/$file
+echo '# END #' >> $dir/$file
+
+# move this script named "template" to /bin/ and write in the bash "template", now you can use the script as a template
+
