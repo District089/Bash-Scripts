5e2402ff9dee22c4d66276339044b79f4ce05f35
Dennis Uhlemann <dennis.uhlemann@web.de>
2022-04-29 15:56:50 +0000

    Bash Script

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@
 
 git add -A
 git commit -m "Bash Script"
-git push https://github.com/District089/Bash-Scripts.git << EOF
+git push https://github.com/District089/Bash-Scripts.git <<EOF
 district089
 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
 EOF
